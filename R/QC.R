200e01b3c7f3741ff103f1a854808958f04b47fd
s1060046 <s1060046@gmail.com>
2020-11-14 10:39:11 +0000

    stability function changed

--- R/QC.R
+++ R/QC.R
@@ -3,19 +3,22 @@
 #' plot
 #' @param file_path, file path of XLS file
 #' @param rig, Rig number
-#' @param stim_sweep, sweep number at DHPG sti
+#' @param stim_sweep, sweep number at DHPG stim
 #' @param res_file, output file name
+#' @param first10vssecond10 first 10 mins - second 10 mins default = 10
+#' @param lm_no deviation calculated from lm baseline default = 5
+#' @param stability_test test for stability, if error is above it will fail default = 7
 #' @return plot
 #' @export
 #'
 
 
-QC <- function(file_path, rig, stim_sweep, res_file){
+QC <- function(file_path, rig, stim_sweep, res_file, first10vssecond10 = 10, lm_no = 7, stability_test = 7){
   if (!dir.exists("./QCdata")){
     dir.create(path = "./QCdata")
   }
   dat <- read_data(file_path = file_path, rig =rig)
   dat_c <- clean_data(recording = dat, stim_sweep = stim_sweep)
   res_file = paste("./QCdata/", res_file, sep = "")
-  QCplot(dat_c, res_file)
+  QCplot(dat_c, res_file, first10vssecond10 = first10vssecond10, lm_no = lm_no, stability_test = stability_test)
 }
